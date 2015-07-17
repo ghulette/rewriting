55f757eeed8fc42673c7e9ccd0cb2cb8920d4254
Geoff Hulette <ghulette@gmail.com>
2015-07-17 13:51:54 -0700

    Update

--- SystemS.v
+++ SystemS.v
@@ -17,6 +17,8 @@ Notation "s1 <+ s2" :=
   (SLeftChoice s1 s2) (at level 80, right associativity).
 
 Inductive eval (A : Set) : strategy A -> A -> option A -> Prop :=
+| E_Rule1 : forall r t t', r t = Some t' -> eval (SRule r) t (Some t')
+| E_Rule2 : forall r t, r t = None -> eval (SRule r) t None
 | E_Id : forall t, eval (SIdentity _) t (Some t)
 | E_Fail : forall t, eval (SFailure _) t None
 | E_Test1 : forall s t t', eval s t (Some t') -> eval (STest s) t (Some t)
@@ -64,3 +66,71 @@ Inductive eval (A : Set) : strategy A -> A -> option A -> Prop :=
       eval s1 t None ->
       eval s2 t None ->
       eval (SChoice s1 s2) t None.
+
+Definition succeeds A (s : strategy A) (t : A) : Prop :=
+  exists t', eval s t (Some t').
+
+Definition fails A (s : strategy A) (t : A) : Prop :=
+  eval s t None.
+
+(*
+Fixpoint without_choice A (s : strategy A) : bool :=
+  match s with
+    | SRule _ => true
+    | SIdentity => true
+    | SFailure => true
+    | STest s => without_choice s
+    | SNeg s => without_choice s
+    | SSeq s1 s2 => andb (without_choice s1) (without_choice s2)
+    | SLeftChoice s1 s2 => andb (without_choice s1) (without_choice s2)
+    | SChoice _ _ => false
+  end.
+*)
+
+Inductive without_choice A : strategy A -> Prop :=
+| WC_Rule : forall r, without_choice (SRule r)
+| WC_Id : without_choice (SIdentity _)
+| WC_Fail : without_choice (SFailure _)
+| WC_Test : forall s, without_choice s -> without_choice (STest s)
+| WC_Neg : forall s, without_choice s -> without_choice (SNeg s)
+| WC_Seq :
+    forall s1 s2,
+      without_choice s1 ->
+      without_choice s2 ->
+      without_choice (s1;s2)
+| WC_LeftChoice :
+    forall s1 s2,
+      without_choice s1 ->
+      without_choice s2 ->
+      without_choice (s1 <+ s2).
+
+Definition deterministic A (s : strategy A) : Prop :=
+  forall t x y, eval s t x -> eval s t y -> x = y.
+
+Theorem without_choice_deterministic :
+  forall A (s : strategy A),
+    without_choice s -> deterministic s.
+Proof.
+  unfold deterministic.
+  induction s; intros WC t x y Hx Hy;
+  inversion WC; subst;
+
+  (* SRule, SIdentity, SFailure *)
+  try (inversion Hx; inversion Hy; subst; congruence).
+
+  (* STest *)
+  try (inversion Hx; inversion Hy; auto; subst;
+       assert (Some t' = None); apply IHs with t; congruence).
+
+  (* SNeg *)
+  inversion Hx; inversion Hy; auto; subst;
+  assert (Some t' = None); apply IHs with t; congruence.
+
+  (* SSeq *)
+  
+
+Lemma fact1:
+  forall A (s : strategy A) t, succeeds s t -> ~fails s t.
+Proof.
+
+  
